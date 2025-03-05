982bf2d71e190d03a83d9808d4e1ed5d66cb1f90
akshayjha82 <akshayjha8263@gmail.com>
2025-03-05 21:24:30 +0530

    first commit

---
+++ src/components/HoverCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { HoverEffect } from "./ui/card-hover-effect";
+
+function HoverCard() {
+  return (
+    <div className=''>
+      <div className=''>
+        <div className='mt-10 text-center'>
+          <h1>hvcds</h1>
+          <p>hzgdvhjd</p>
+        </div>
+        <div className='mt-10'>2</div>
+        <div className='mt-10 text-center'>3</div>
+      </div>
+    </div>
+  )
+}
+
+export default HoverCard
